9695da7055595f96a63c7f8e3679cf9d4d840d97
mevey <muthoni90@gmail.com>
2018-11-05 09:21:32 -0800

    cleaner function

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ def speakers():
 
 	format_ = request.args.get("format", None)
 	speaker = request.args.get("name", "")
-	year = request.args.get("year", "")
 
 	connection = sqlite3.connect("mydatabase.sqlite") 
 	connection.row_factory = dictionary_factory
@@ -46,44 +45,19 @@ def speakers():
 				on speaker.speech_id = speech.speech_id %s %s;"
 
 	where_clause = ""
-	if speaker or year:
-		where_clause = "where "
-		if speaker:
-			where_clause += " speaker.surname = ? " if speaker else ""
-		if year and speaker:
-			where_clause += " and "
-		if year:
-			where_clause += " hearing.date like ? " if len(year)>2 else "" 
+	if speaker:
+		where_clause = "where speaker.surname = ? " if speaker else ""
+
 	limit_statement = "limit 20" if format_ != "csv" else ""
 
 	all_records_query = all_records_query % (where_clause, limit_statement)
-	print(all_records_query)
 
-	if speaker and year:
-		cursor.execute(all_records_query ,(speaker.lower(), "%"+ year))
-	elif speaker:
+	if speaker:
 		cursor.execute(all_records_query ,(speaker.lower(),))
-	elif year:
-		cursor.execute(all_records_query ,("%"+ year,))
 	else:
 		cursor.execute(all_records_query)
 	records = cursor.fetchall()
 
-	#Query to count the number of records
-	count_query =  "SELECT count(*) as count FROM hearing inner join speech on \
-				speech.hearing_id = hearing.hearing_id inner join speaker \
-				on speaker.speech_id = speech.speech_id %s;"
-	count_query = count_query % (where_clause)
-	if speaker and year:
-		cursor.execute(count_query, (speaker.lower(), "%"+ year))
-	elif year:
-		cursor.execute(count_query, ("%"+ year,))
-	elif speaker:
-		cursor.execute(count_query, (speaker.lower()))
-	else:
-		cursor.execute(count_query)
-	#There's a lot of if else going on here but I will send a better solution for you guys to work with
-	no_of_records = cursor.fetchall()
 	connection.close()
 
 	#Send the information back to the view
@@ -92,8 +66,7 @@ def speakers():
 		return download_csv(records, "speeches_%s.csv" % (speaker.lower()))
 	else:
 		years = [x for x in range(2018, 1995, -1)]
-		selected_year = int(year) if year else None
-		return flask.render_template('speaker.html', records=records, no_of_records=no_of_records[0]['count'], speaker=speaker, years=years, selected_year=selected_year)
+		return flask.render_template('speaker.html', records=records, speaker=speaker, years=years)
 
 ########################################################################
 # The following are helper functions. They do not have a @app.route decorator
